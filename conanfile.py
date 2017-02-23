2db87579ed3781dc5ff8849c583af602210169e0
Емцев Александр Сергеевич <emtsev@win.ftc.ru>
2017-02-23 15:44:42 +0300

    conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, CMake
+
+class HelloConan(ConanFile):
+    description = "Demo package"
+    license = "lic"
+    url = "https://github.com/Defvyb/test_conan_proj"
+    name = "hello"
+    version = "0.1"
+    exports = "include/*", "src/*", "CMakeLists.txt"
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake "{}" {}'.format(self.conanfile_directory,
+                                        cmake.command_line))
+        self.run('cmake --build . {}'.format(cmake.build_config))
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*.lib", dst="lib", src="lib")
+        self.copy("*.a", dst="lib", src="lib")
+#dependencies
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
